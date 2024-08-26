e022c9132a4f63c0bf3c19cef5a253e4a4fd6ff1
junwlee <juns971229@gmail.com>
2024-08-26 10:09:05 +0900

    BackEnd and FrontEnd initial settings

---
+++ src/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views"); // __dirname은 프로젝트의 루트 디렉토리
+
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/")); // catchall url
+
+const port = 3000;
+const handleListen = () => console.log(`Listening on http://localhost:${port}`);
+app.listen(port, handleListen);
