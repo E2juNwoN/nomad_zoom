1b0a3908a95ac8acbd79af78e0b7c187fee5be45
junwlee <juns971229@gmail.com>
2024-08-27 11:02:18 +0900

    브라우저에서 서버로 메시지를 전송

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -3,6 +3,9 @@ const messageList = document.querySelector("ul");
 const messageForm = document.querySelector("form");
 messageForm.addEventListener("submit", (event) => {
     event.preventDefault();
+    const input = messageForm.querySelector("input");
+    socket.send(input.value);
+    input.value = "";
 });
 
 const socket = new WebSocket(`ws://${window.location.host}`);
